cf3c1f038488dfadb8964cb0d3c35b4d2cc35630
徐海静 <2318644781@qq.com>
2023-09-08 18:53:33 +0800

    first commit

---
+++ src/main/java/com/hex/saleshealthrecordingsystem/SalesHealthRecordingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hex.saleshealthrecordingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesHealthRecordingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesHealthRecordingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hex/saleshealthrecordingsystem/SalesHealthRecordingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hex.saleshealthrecordingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesHealthRecordingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
